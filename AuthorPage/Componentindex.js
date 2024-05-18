c2647fc6d741d3e3b24e54dc1f518d90091dd664
VishalGurudasani <145326794+VishalGurudasani@users.noreply.github.com>
2024-05-18 10:02:39 +0530

    Update Componentindex.js

--- AuthorPage/Componentindex.js
+++ AuthorPage/Componentindex.js
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:68ea875a1f1c2a5be19b631579b482a28af10646e35623123cb21817b038515a
-size 253
+import AuthorNFTCardBox from "./AuthorNFTCardBox/AuthorNFTCardBox";
+import AuthorProfileCard from "./AuthorProfileCard/AuthorProfileCard";
+import AuthorTaps from "./AuthorTaps/AuthorTaps";
+
+
+export {AuthorNFTCardBox, AuthorProfileCard, AuthorTaps};
