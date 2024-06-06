d94cfa3b2d2306911890ee627e7efae30d54fb2b
VishalGurudasani <145326794+VishalGurudasani@users.noreply.github.com>
2024-06-06 15:26:19 +0530

    Update Collectionindex.js

--- CollectionPage/Collectionindex.js
+++ CollectionPage/Collectionindex.js
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:86fb62982708f68b5a9f65dd30825bbce5a566b41b99232e420dcf931c814255
-size 209
+import CollectionProfile from "./CollectionProfile/CollectionProfile";
+import Banner from "./Banner/Banner";
+import NFTCardTwo from "./NFTCard2/NFTCard2";
+
+export { CollectionProfile, Banner, NFTCardTwo };
