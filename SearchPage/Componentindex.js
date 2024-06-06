d58eeabcf64c49be266d91353f797f88dd951900
VishalGurudasani <145326794+VishalGurudasani@users.noreply.github.com>
2024-06-06 15:27:22 +0530

    Update Componentindex.js

--- SearchPage/Componentindex.js
+++ SearchPage/Componentindex.js
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9fdb0d94cbff6b300a3c1f732b4a974a4d1f6540fae9cbd86432d10737d36229
-size 69
+import SearchBar from "./SearchBar/SearchBar";
+
+export {SearchBar};
