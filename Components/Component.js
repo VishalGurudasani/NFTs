27891aa861c77d0239ccfe10e817ce6bb6b02375
VishalGurudasani <145326794+VishalGurudasani@users.noreply.github.com>
2024-06-06 15:36:13 +0530

    Update Component.js

--- Components/Component.js
+++ Components/Component.js
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e0568cf9d5c44dd4d05df6c463df705d9e1414bcef71296d28cb3bbfd4648471
-size 956
+import NavBar from "./NavBar/NavBar";
+import Button from "./Button/Button";
+import Footer from "./Footer/Footer";
+import Hsection from "./HSection/Hsection";
+import Service from "./Service/Service";
+import Slider from "./Slider/Slider";
+import Subscribe from "./Subscribe/Subscribe";
+import Title from "./Title/Title";
+import Category from "./Category/Category";
+import Filter from "./Filter/Filter";
+import NFTCard from "./NFTCard/NFTCard";
+import Collection from "./Collecttion/Collection";
+import LikeProfile from "./LikeProfile/LikeProfile";
+import Audio from "./Audio/Audio";
+import Follower from "./Follower/Follower";
+import Slider2 from "./Slider2/Slider2";
+import Brand from "./Brand/Brand";
+import Video from "./Video/Video";
+import Loader from "./Loader/Loader";
+
+export {NavBar,Button,Footer,Hsection,Service,Slider,Subscribe,Title,Category,Filter,NFTCard,Collection,LikeProfile,Audio,Follower,Slider2,Brand,Video,Loader};
