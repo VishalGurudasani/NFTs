5118e9cd61034dd2724b83b3a146ff6cdef5cdf1
VishalGurudasani <145326794+VishalGurudasani@users.noreply.github.com>
2024-06-06 15:46:27 +0530

    Update Home.js

--- app/Home.js
+++ app/Home.js
@@ -1,3 +1,42 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3bf654b85cc5b04c0b8afacfaadbd16b81382930e33170e44c4f91fcb9259cc0
-size 1221
+'use client';
+import React, { useContext, useEffect } from 'react'
+import { Follower, NFTCard, NavBar } from "../Components/Component";
+import { Footer } from '../Components/Component';
+import Style from './page.module.css'
+import { Hsection,Service,Slider,Subscribe, Title ,Category ,Filter, Collection, Audio, Slider2, Brand, Video} from '../Components/Component';
+
+import {NFTMarketPlaceContext} from '@/Context/NFTMarketPlaceContext';
+const Home = () => {
+  const { checkWallet } = useContext(NFTMarketPlaceContext);
+ useEffect(()=>{
+  checkWallet();
+ },[]);
+  
+  return (
+    <div>
+      
+      <NavBar suppressHydrationWarning={true}/>
+      <div className={Style.homepage}>
+        <Hsection/>
+        <Service/>
+        <Slider/>
+        <Title heading="Browse By Category" paragraph="Explore NFTs in the most featured categories"/>
+        <Category/>
+        <Filter/>
+        <NFTCard/>
+        <Audio/>
+        <Title heading="Browse By Category" paragraph="Follower Section"/>
+        <Follower/>
+        <Slider2/>
+        <Collection/>
+        <Subscribe/>
+        <Brand/>
+        <Video/>
+      </div>
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default Home;
