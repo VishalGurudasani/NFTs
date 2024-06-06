1c135319885b44e4c63b26328a5f0dd862b689c8
VishalGurudasani <145326794+VishalGurudasani@users.noreply.github.com>
2024-06-06 15:26:53 +0530

    Update NFTDetailsindex.js

--- NFTDetailsPage/NFTDetailsindex.js
+++ NFTDetailsPage/NFTDetailsindex.js
@@ -1,3 +1,5 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:835ed362895a642e59fd2520582f93faa34d1267ae181bdecb121bff060e6b35
-size 217
+import NFTDescription from "./NFTDescription/NFTDescription";
+import NFTDetailsimg from "./NFTDetailsImg/NFTDetailsimg";
+import NFTTabs from "./NFTTabs/NFTTabs";
+
+export { NFTDescription, NFTDetailsimg, NFTTabs };
