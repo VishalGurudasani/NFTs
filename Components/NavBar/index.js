fde07c96cdfdae04419a182adf798cf5984a70f9
VishalGurudasani <145326794+VishalGurudasani@users.noreply.github.com>
2024-06-06 15:40:39 +0530

    Update index.js

--- Components/NavBar/index.js
+++ Components/NavBar/index.js
@@ -1,3 +1,7 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e08c6e1d6a141f32e8d217efbc3940ad44863f98ecc1caffe2dbb32bb3542052
-size 272
+import Discover from "./Discover/Discover";
+import Help from "./Help/Help";
+import Notification from "./Notification/Notification";
+import Profile from "./Profile/Profile";
+import SideBar from "./SideBar/SideBar";
+
+export{Discover,Help,Notification,Profile,SideBar};
