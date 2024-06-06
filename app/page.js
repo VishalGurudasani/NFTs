8dbf7659607ec1639d85d3e08d472b467811e84c
VishalGurudasani <145326794+VishalGurudasani@users.noreply.github.com>
2024-06-06 15:44:51 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:59e83086a2b068222ae71adb75bec9a16628aec5b95c5e32519d8a5a0675140d
-size 285
+import React from 'react'
+import Home from './Home'
+import { NFTMarketPlaceProvider } from '../Context/NFTMarketPlaceContext';
+
+const page = () => {
+  return (
+    <div>
+    <NFTMarketPlaceProvider>
+       <Home/>
+       </NFTMarketPlaceProvider>
+    </div>
+  )
+}
+
+export default page;
