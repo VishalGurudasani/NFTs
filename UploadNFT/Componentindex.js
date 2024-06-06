d2795cc4cbd312d7286e99ffa68877930caab4fa
VishalGurudasani <145326794+VishalGurudasani@users.noreply.github.com>
2024-06-06 15:28:04 +0530

    Update Componentindex.js

--- UploadNFT/Componentindex.js
+++ UploadNFT/Componentindex.js
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d6b3b5faa2027b5fb7329c553e1ae5954a2140f64368874b28a22beff470622e
-size 61
+import UploadNFT from "./UploadNFT";
+
+
+export {UploadNFT};
